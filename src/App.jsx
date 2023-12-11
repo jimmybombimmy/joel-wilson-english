bfbc7d4d70040b5490ee3bba06239530645505ad
jimmybombimmy <jamespeters92@gmail.com>
2023-12-11 12:41:56 +0000

    first commit: create basic bootstrap navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import NavFile from './navbar/NavFile.jsx'
+
+function App() {
+
+  return (
+    <>
+    <NavFile expand="lg"/>
+    {/* <Routes>
+
+    </Routes> */}
+    </>
+  )
+}
+
+export default App
