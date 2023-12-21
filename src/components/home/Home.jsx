134ca7a9b56dc569b21eef3918a435e398025ed8
jimmybombimmy <jamespeters92@gmail.com>
2023-12-21 00:30:14 +0000

    just X Boxes add info and decorate images around it

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,6 +6,12 @@ import triangleArrows from "../../img/triangle-arrows.png"
 import grammarBubble from "../../img/grammar-bubble.png"
 import speakBubble from "../../img/speak-bubble.png"
 import vocabBubble from "../../img/vocabulary-bubble.png"
+import leftDots from "../../img/left-dots.png"
+import middleDots from "../../img/middle-dots.png"
+import rightDots from "../../img/right-dots.png"
+import leftStar from "../../img/left-star.png"
+import middleStar from "../../img/middle-star.png"
+import rightStar from "../../img/right-star.png"
 
 function Home() {
   return (
@@ -35,13 +41,28 @@ function Home() {
           <h2 id="chooseYourClassTitle">Choose your class!</h2>
           <section id="justXGrouped">
             <section className="justXSections justGrammarSection">
-              <img src={grammarBubble} alt="Grammar speech bubble" />
+              <img id="leftStar" src={leftStar} alt="" />
+              <img id="leftDots" src={leftDots} alt="" />
+              <img className="justBubbles" src={grammarBubble} alt="Grammar speech bubble" />
+              <h3>Just Grammar!</h3>
+              <p>Learn the grammar, complete activities to understand them correctly with speaking activities in groups to practice them.</p>
+              {/* <button>Book Now!</button> */}
             </section>
             <section className="justXSections justSpeakSection">
-              <img src={speakBubble} alt="Speak speech bubble" />
+              <img id="middleStar" src={middleStar} alt="" />
+              <img id="middleDots" src={middleDots} alt="" />
+              <img className="justBubbles" src={speakBubble} alt="Speak speech bubble" />
+              <h3>Just Speak!</h3>
+              <p>Practice speaking about common topics and get lots of feedback on your pronunciation, grammar, and vocabulary. </p>
+              {/* <button>Book Now!</button> */}
             </section>
             <section className="justXSections justVocabSection">
-              <img src={vocabBubble} alt="Vocabulary speech bubble" />
+              <img id="rightStar" src={rightStar} alt="" />
+              <img id="rightDots" src={rightDots} alt="" />
+              <img className="justBubbles" src={vocabBubble} alt="Vocabulary speech bubble" />
+              <h3>Just Vocabulary!</h3>
+              <p>Increase your vocabulary with new words. Complete with exercises and feedback every lesson to make sure you get it right.</p>
+              {/* <button>Book Now!</button> */}
             </section>
           </section>
         </section>
