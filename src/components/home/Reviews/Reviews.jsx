afff4025bc7197aaa0d1f1ee6225661557a39446
jimmybombimmy <jamespeters92@gmail.com>
2024-01-26 18:31:28 +0000

    reviews section resized to one column for smaller widths

--- src/components/home/Reviews/Reviews.jsx
+++ src/components/home/Reviews/Reviews.jsx
@@ -1,10 +1,16 @@
 import "./Reviews.css";
-import { reviewsArray, reviewsCircle, reviewsLoop, reviewsDots } from "../../../assets/home/home-images";
+import "../../App.css";
+import {
+  reviewsArray,
+  reviewsCircle,
+  reviewsLoop,
+  reviewsDots,
+} from "../../../assets/home/home-images";
 
 import { Carousel } from "react-bootstrap";
 
 export default function ReviewsSection() {
-  const baseArray = [];
+  const baseArrayInFour = [];
 
   const reviewsInFour = (reviewsArray) => {
     let startNum = 0;
@@ -12,7 +18,7 @@ export default function ReviewsSection() {
     const grabFour = () => {
       if (endNum > reviewsArray.length) return;
       const arrOfFour = reviewsArray.slice(startNum, endNum);
-      baseArray.push(arrOfFour);
+      baseArrayInFour.push(arrOfFour);
       startNum += 4;
       endNum += 4;
       grabFour();
@@ -20,46 +26,105 @@ export default function ReviewsSection() {
     grabFour();
   };
 
+  const baseArrayInTwo = [];
+
+  const reviewsInTwo = (reviewsArray) => {
+    let startNum = 0;
+    let endNum = 2;
+    const grabTwo = () => {
+      if (endNum > reviewsArray.length) return;
+      const arrOfTwo = reviewsArray.slice(startNum, endNum);
+      baseArrayInTwo.push(arrOfTwo);
+      startNum += 2;
+      endNum += 2;
+      grabTwo();
+    };
+    grabTwo();
+  };
+
   reviewsInFour(reviewsArray);
+  reviewsInTwo(reviewsArray);
 
   return (
     <section id="reviewsSection">
       <h2 id="reviewsTitle">What students have to say</h2>
-      <Carousel data-bs-theme="dark" indicators={false} interval={null}>
-        {baseArray.map((selection) => {
-          let i = 0;
-          return (
-            <Carousel.Item>
-              <div className="gridContainer">
-                {selection.map((imgPath) => {
-                  i++;
-                  return (
-                    <div className="reviewImg">
-                      <img className={`reviewImg${i}Of4 reviewImgFront`} src={imgPath} />
-                    </div>
-                  );
-                })}
-              </div>
-            </Carousel.Item>
-          );
-        })}
-      </Carousel>
-      <div className="gridContainer backgroundGridContainer">
-        <div className="reviewImg1Of4">
-          <img className="reviewsCircle" src={reviewsCircle} alt="" />
+      <div className="fourReviewsSection hideBelow767">
+        <Carousel data-bs-theme="dark" indicators={false} interval={null}>
+          {baseArrayInFour.map((selection) => {
+            let i = 0;
+            return (
+              <Carousel.Item>
+                <div className="gridContainer">
+                  {selection.map((imgPath) => {
+                    i++;
+                    return (
+                      <div className="reviewImg">
+                        <img
+                          className={`reviewImg${i}Of4 reviewImgFront`}
+                          src={imgPath}
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+        <div className="gridContainer backgroundGridContainer">
+          <div className="reviewImg1Of4">
+            <img className="reviewsCircle" src={reviewsCircle} alt="" />
+          </div>
+          <div className="reviewImg2Of4">
+            <img className="reviewsLoop" src={reviewsLoop} alt="" />
+          </div>
+          <div className="reviewImg3Of4"></div>
+          <div className="reviewImg4Of4">
+            <img className="reviewsDots" src={reviewsDots} />
+          </div>
         </div>
-        <div className="reviewImg2Of4">
-          <img className="reviewsLoop" src={reviewsLoop} alt="" />
-        </div>
-        <div className="reviewImg3Of4"></div>
-        <div className="reviewImg4Of4">
-          <img className="reviewsDots" src={reviewsDots} />
-        </div>
-      </div>
-      {/* {homeImg.reviewsArray.map((review) => {
+        {/* {homeImg.reviewsArray.map((review) => {
             console.log(review);
             return <img src={review} />;
           })} */}
+      </div>
+      <div className="twoReviewsSection hideAbove768">
+        <Carousel data-bs-theme="dark" indicators={false} interval={null}>
+          {baseArrayInTwo.map((selection) => {
+            let i = 0;
+            return (
+              <Carousel.Item>
+                <div className="gridContainer">
+                  {selection.map((imgPath) => {
+                    i++;
+                    return (
+                      <div className="reviewImg">
+                        <img
+                          className={`reviewImg${i}Of4 reviewImgFront`}
+                          src={imgPath}
+                        />
+                      </div>
+                    );
+                  })}
+                </div>
+              </Carousel.Item>
+            );
+          })}
+        </Carousel>
+        <div className="gridContainer backgroundGridContainer">
+          <div className="reviewImg1Of4">
+            <img className="reviewsCircle" src={reviewsCircle} alt="" />
+          </div>
+          <div className="reviewImg2Of4">
+            <img className="reviewsLoop" src={reviewsLoop} alt="" />
+            <img className="reviewsDots" src={reviewsDots} />
+          </div>
+        </div>
+        {/* {homeImg.reviewsArray.map((review) => {
+            console.log(review);
+            return <img src={review} />;
+          })} */}
+      </div>
     </section>
   );
 }
