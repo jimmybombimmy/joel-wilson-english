4ce5406bcff1a5727e21bc878b2954d2196a2885
jimmybombimmy <jamespeters92@gmail.com>
2024-01-28 11:22:09 +0000

    changed reviews array into imports to attempt to fix rendering issue

--- src/assets/home/home-images.js
+++ src/assets/home/home-images.js
@@ -26,12 +26,98 @@ import reviewsLoop from "./reviews-loop.png"
 import michaelFranks from "./michael-franks.png"
 import joelLinkedIn from "./joel-linkedin.jpeg"
 
-const reviewsArray = []
+import review1 from "./src/assets/home/reviews/review1.jpg";
+import review2 from "./src/assets/home/reviews/review2.jpg";
+import review3 from "./src/assets/home/reviews/review3.jpg";
+import review4 from "./src/assets/home/reviews/review4.jpg";
+import review5 from "./src/assets/home/reviews/review5.jpg";
+import review6 from "./src/assets/home/reviews/review6.jpg";
+import review7 from "./src/assets/home/reviews/review7.jpg";
+import review8 from "./src/assets/home/reviews/review8.jpg";
+import review9 from "./src/assets/home/reviews/review9.jpg";
+import review10 from "./src/assets/home/reviews/review10.jpg";
+import review11 from "./src/assets/home/reviews/review11.jpg";
+import review12 from "./src/assets/home/reviews/review12.jpg";
+import review13 from "./src/assets/home/reviews/review13.jpg";
+import review14 from "./src/assets/home/reviews/review14.jpg";
+import review15 from "./src/assets/home/reviews/review15.jpg";
+import review16 from "./src/assets/home/reviews/review16.jpg";
+import review17 from "./src/assets/home/reviews/review17.jpg";
+import review18 from "./src/assets/home/reviews/review18.jpg";
+import review19 from "./src/assets/home/reviews/review19.jpg";
+import review20 from "./src/assets/home/reviews/review20.jpg";
+import review21 from "./src/assets/home/reviews/review21.jpg";
+import review22 from "./src/assets/home/reviews/review22.jpg";
+import review23 from "./src/assets/home/reviews/review23.jpg";
+import review24 from "./src/assets/home/reviews/review24.jpg";
+import review25 from "./src/assets/home/reviews/review25.jpg";
+import review26 from "./src/assets/home/reviews/review26.jpg";
+import review27 from "./src/assets/home/reviews/review27.jpg";
+import review28 from "./src/assets/home/reviews/review28.jpg";
+import review29 from "./src/assets/home/reviews/review29.jpg";
+import review30 from "./src/assets/home/reviews/review30.jpg";
+import review31 from "./src/assets/home/reviews/review31.jpg";
+import review32 from "./src/assets/home/reviews/review32.jpg";
+import review33 from "./src/assets/home/reviews/review33.jpg";
+import review34 from "./src/assets/home/reviews/review34.jpg";
+import review35 from "./src/assets/home/reviews/review35.jpg";
+import review36 from "./src/assets/home/reviews/review36.jpg";
+import review37 from "./src/assets/home/reviews/review37.jpg";
+import review38 from "./src/assets/home/reviews/review38.jpg";
+import review39 from "./src/assets/home/reviews/review39.jpg";
+import review40 from "./src/assets/home/reviews/review40.jpg";
+import review41 from "./src/assets/home/reviews/review41.jpg";
+import review42 from "./src/assets/home/reviews/review42.jpg";
+import review43 from "./src/assets/home/reviews/review43.jpg";
+import review44 from "./src/assets/home/reviews/review44.jpg";
 
-for(let i = 1; i <= 44; i++) {
-  reviewsArray.push(`./src/assets/home/reviews/review${i}.jpg`)
-}
 
+const reviewsArray = [
+  review1,
+  review2,
+  review3,
+  review4,
+  review5,
+  review6,
+  review7,
+  review8,
+  review9,
+  review10,
+  review11,
+  review12,
+  review13,
+  review14,
+  review15,
+  review16,
+  review17,
+  review18,
+  review19,
+  review20,
+  review21,
+  review22,
+  review23,
+  review24,
+  review25,
+  review26,
+  review27,
+  review28,
+  review29,
+  review30,
+  review31,
+  review32,
+  review33,
+  review34,
+  review35,
+  review36,
+  review37,
+  review38,
+  review39,
+  review40,
+  review41,
+  review42,
+  review43,
+  review44
+];
 
 
 export { michaelInABox, learnUnderline, wavyArrows, triangleArrows, grammarBubble, speakBubble, vocabBubble, leftDots, middleDots, rightDots, leftStar, middleStar, rightStar, oneToOneImg, groupImg, oneToOneSquiggle, groupSquiggle, eclipse1, eclipse2, reviewsArray, reviewsCircle, reviewsDots, reviewsLoop, michaelFranks, joelLinkedIn, grammarBubbleReverse, speakBubbleReverse, vocabBubbleReverse };
