008e3c40318a5cfb6ab68c5918702d190ee6dc6e
jimmybombimmy <jamespeters92@gmail.com>
2024-02-16 16:12:55 +0000

    add bottom section + change of colour to method boxes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,13 @@
 import AboutIntro from "./AboutIntro/AboutIntro";
 import AboutMethod from "./AboutMethod/AboutMethod";
+import AboutBottom from "./AboutBottom/AboutBottom";
 
 export default function About() {
   return (
     <main id="aboutPage">
       <AboutIntro />
       <AboutMethod />
+      <AboutBottom />
     </main>
   )
 }
---
+++ src/components/About/AboutBottom/AboutBottom.jsx
@@ -0,0 +1,45 @@
+import "./AboutBottom.css";
+
+export default function AboutBottom() {
+  return (
+    <section id="aboutBottomSection">
+      <div id="aboutBottomInner">
+        <h2 id="aboutBottomTitle">My Classes</h2>
+        <div>
+          <p>
+            I offer both fully personalised English lessons and also group
+            classes of up to 7 people.
+          </p>
+          <p>
+            All of my services can be found on my website: <a href="www.joelwilson.as.me">www.joelwilson.as.me</a>
+          </p>
+          <p>A <u style={{color: "#C1688A"}}>30-minute trial</u> is available on request!</p>
+        </div>
+        <div>
+          <p>
+            I also upload guides and materials to my YouTube channel and my
+            Instagram:
+          </p>
+          <p>
+            <strong>YouTube:</strong>{" "}
+            <a href="https://www.youtube.com/@JoelEdwardIanWilson/videos">
+              @JoelEdwardIanWilson
+            </a>
+          </p>
+          <p>
+            <strong>Instagram:</strong>{" "}
+            <a href="https://www.instagram.com/joel.edward.ian.wilson/">
+              joel.edward.ian.wilson
+            </a>
+          </p>
+          <p>
+            <strong>TikTok:</strong>{" "}
+            <a href="https://www.tiktok.com/@joel.edward.ian.wilson">
+              joel.edward.ian.wilson
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
