7cb988831672b3337ad806fa324084f5c101a92a
jimmybombimmy <jamespeters92@gmail.com>
2024-02-20 18:12:18 +0000

    create ArticlesPage first draft for desktop

--- src/components/Articles/ArticlesPage.jsx
+++ src/components/Articles/ArticlesPage.jsx
@@ -1,5 +1,8 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { getArticles } from "../../utils/api-calls";
+import { extractArticleBodyPreview } from "../../utils/article-utils";
+
+import "./ArticlesPage.css";
 
 export default function ArticlesPage() {
   const [allArticlesData, setAllArticleData] = useState(false);
@@ -7,17 +10,56 @@ export default function ArticlesPage() {
   useEffect(() => {
     if (!allArticlesData) {
       getArticles().then(({ data }) => {
-        return setAllArticleData(data.data);
+        return setAllArticleData([
+          data.data[0],
+          data.data[0],
+          data.data[0],
+          data.data[0],
+          data.data[0],
+        ]);
       });
     }
   }, []);
 
   return (
-    <main>
-      <h1>Articles</h1>
-    {allArticlesData ? (allArticlesData.map((stuff) => {
-      return <h5><a href={`/articles/${stuff.id}`}> {stuff.attributes.title}</a> - {stuff.id}</h5>
-    })) : (<p>Waiting for articles</p>)}
+    <main id="articlesPage">
+      <p id="articleCountInfo">
+        {allArticlesData
+          ? `Total Articles: ${allArticlesData.length}`
+          : `Waiting for articles`}
+      </p>
+      <h1 id="articlesPageTitle">Latest Articles</h1>
+
+      <section id="articleBoxesSection">
+        {allArticlesData ? (
+          allArticlesData.map((articleInfo) => {
+            console.log(articleInfo);
+            return (
+              <article className="articleBox" key={`article${articleInfo.id}`}>
+                <img
+                  className="articleBoxImg"
+                  src={articleInfo.attributes.img1}
+                  alt={articleInfo.attributes.alt1}
+                />
+                <div className="articleBoxInfo">
+                  <p className="articleBoxCreatedAt">{`Article Created: ${articleInfo.attributes.updatedAt}`}</p>
+                  <h3 className="articleBoxTitle">
+                    <a href={`/articles/${articleInfo.id}`}>
+                      {" "}
+                      {articleInfo.attributes.title}
+                    </a>{" "}
+                  </h3>
+                  <p className="articleBoxText">
+                    {extractArticleBodyPreview(articleInfo.attributes.body1)}
+                  </p>
+                </div>
+              </article>
+            );
+          })
+        ) : (
+          <p>Waiting for articles</p>
+        )}
+      </section>
     </main>
-  )
+  );
 }
